af5c1e06210b693960c2afaeee75ea259301accc
Nafiul <nafiulhaque22@gmail.com>
2022-05-07 20:41:24 +0600

    services and works on design

--- src/Components/Pages/Services/Services.js
+++ src/Components/Pages/Services/Services.js
@@ -1,9 +1,63 @@
 import "./Services.css";
-import React from 'react'
+import React from 'react';
+import cover1 from "../../../img/I will install and setup wordpress with demo theme 1.jpg";
+import cover2 from "../../../img/I will install and setup wordpress with demo theme.jpg";
+import cover3 from "../../../img/I will provide psd or xd to html css bootstrap responsive website.jpg";
 
 const Services = () => {
     return (
-        <div>Services</div>
+        <div id="services">
+            <div className="header">
+                <h1>Services</h1>
+            </div>
+            <div className="box__grp">
+                <div className="box my-5">
+                    <div className="items hvr-grow">
+                        <img className="img" src={cover1} />
+                        <div className="order hvr-buzz">
+                            <a href="">Order Now</a>
+                        </div>
+                        <div className="text">
+                            <h5>install and setup wordpress with demo</h5>
+                        </div>
+                    </div>
+                    <div className="items hvr-grow">
+                        <img className="img" src={cover2}
+                        />
+
+                        <div className="order hvr-buzz">
+                            <a href="">Order Now</a>
+                        </div>
+                        <div className="text">
+                            <h5>provide psd or xd to html css bootstrap responsive website</h5>
+                        </div>
+                    </div>
+                    <div className="items hvr-grow">
+                        <img src={cover3} className="img"
+                        />
+
+                        <div className="order hvr-buzz">
+                            <a href="">Order Now</a>
+                        </div>
+                        <div className="text">
+                            <h5>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, officiis?</h5>
+                        </div>
+                    </div>
+                    <div className="items hvr-grow">
+                        <div className="img"
+                        >
+                        </div>
+                        <div className="order hvr-buzz">
+                            <a href="">Order Now</a>
+                        </div>
+                        <div className="text">
+                            <h5>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, officiis?</h5>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
--- src/Components/Pages/Works/Works.js
+++ src/Components/Pages/Works/Works.js
@@ -1,66 +1,71 @@
 import "./Works.css";
-import cover1 from "../../../img/I will install and setup wordpress with demo theme 1.jpg";
-import cover2 from "../../../img/I will install and setup wordpress with demo theme.jpg";
-import cover3 from "../../../img/I will provide psd or xd to html css bootstrap responsive website.jpg";
+import React from 'react';
+import img1 from "../../../img/works-img/custom-web-app.jpg";
+import img2 from "../../../img/works-img/e-commerce.jpg";
+import img3 from "../../../img/works-img/full-stack.jpg";
+import img4 from "../../../img/works-img/wordpress.jpg";
 
 
-import React from 'react'
 
 const Works = () => {
     return (
         <div id="works">
-            <div className="header">
-                <h1>Works on</h1>
-            </div>
-            <div className="box__grp">
-                <div className="box my-5">
-                    <div className="items hvr-grow">
-                        <img className="img" src={cover1} />
-                        <div className="order hvr-buzz">
-                            <a href="">Order Now</a>
-                        </div>
-                        <div className="text">
-                            <h5>install and setup wordpress with demo</h5>
-                        </div>
-                    </div>
-                    <div className="items hvr-grow">
-                        <img className="img" src={cover2}
-                        />
-
-                        <div className="order hvr-buzz">
-                            <a href="">Order Now</a>
-                        </div>
-                        <div className="text">
-                            <h5>provide psd or xd to html css bootstrap responsive website</h5>
-                        </div>
-                    </div>
-                    <div className="items hvr-grow">
-                        <img src={cover3} className="img"
-                        />
-
-                        <div className="order hvr-buzz">
-                            <a href="">Order Now</a>
-                        </div>
-                        <div className="text">
-                            <h5>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, officiis?</h5>
-                        </div>
-                    </div>
-                    <div className="items hvr-grow">
-                        <div className="img"
-                        >
-                        </div>
-                        <div className="order hvr-buzz">
-                            <a href="">Order Now</a>
-                        </div>
-                        <div className="text">
-                            <h5>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, officiis?</h5>
-                        </div>
-                    </div>
+            <div className="plate">
+                <div className="left-p text-green">
+                    <h3>Custom Web Application</h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing
+                        and typesetting industry. Lorem Ipsum has been
+                        the industry's standard dummy text ever since
+                        the 1500s, when an unknown printer took a galley
+                        of type and scrambled it to make a type specimen
+                        book.</p>
+                    <a href="#" className="btn" alt="button">See More</a>
                 </div>
+                <img className="right-p" src={img3} alt="web application" />
+
 
             </div>
+            <div className="plate">
+                <div className="left-p text-green">
+                    <h3>E-commerce</h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing
+                        and typesetting industry. Lorem Ipsum has been
+                        the industry's standard dummy text ever since
+                        the 1500s, when an unknown printer took a galley
+                        of type and scrambled it to make a type specimen
+                        book.</p>
+                    <a href="#" className="btn" alt="button">See More</a>
+                </div>
+                <img className="right-p" src={img2} alt="ecommerce" />
+            </div>
+            <div className="plate">
+                <div className="left-p text-green">
+                    <h3>Full Stack Development</h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing
+                        and typesetting industry. Lorem Ipsum has been
+                        the industry's standard dummy text ever since
+                        the 1500s, when an unknown printer took a galley
+                        of type and scrambled it to make a type specimen
+                        book.</p>
+                    <a href="#" className="btn" alt="button">See More</a>
+                </div>
+                <img className="right-p" src={img1} alt="ecommerce" />
+            </div>
+            <div className="plate">
+                <div className="left-p text-green">
+                    <h3>WordPress Development</h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing
+                        and typesetting industry. Lorem Ipsum has been
+                        the industry's standard dummy text ever since
+                        the 1500s, when an unknown printer took a galley
+                        of type and scrambled it to make a type specimen
+                        book.</p>
+                    <a href="#" className="btn" alt="button">See More</a>
+                </div>
+                <img className="right-p" src={img4} alt="ecommerce" />
+            </div>
 
-        </div>
+        </div >
     )
 }
 
